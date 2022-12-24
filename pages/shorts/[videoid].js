0792327d116c57ccd7cbd9b231dc3948536e7ff4
Divyanshu Kunwar <kdivyanshu3@gmail.com>
2022-12-24 23:25:40 +0530

    another one I spelt as i

--- pages/shorts/[videoid].js
+++ pages/shorts/[videoid].js
@@ -9,14 +9,14 @@ import ShortsVideo from "../../components/ShortsVideo/ShortsVideo"
 
 export async function getStaticPaths() {
     return {
-      paths: [{ params: { videoid: 'home'} }],
+      paths: [{ params: { videoId: 'home'} }],
       fallback: 'blocking',     // can also be true or 'blocking'
     }
   }
 
 export async function getStaticProps(context) {
     return {
-        props: { videoid: context.params.videoid },
+        props: { videoid: context.params.videoId },
     };
 }
 
